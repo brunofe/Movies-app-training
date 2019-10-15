c99349ca263a0c5cd47b002330c8bd5690b1e9e1
brunofe <ferraresibruno@hotmail.com>
2019-10-15 09:35:34 -0300

    saving

---
+++ app/src/main/java/com/example/moviesot/login/loginScreen/loginActivity.java
@@ -0,0 +1,17 @@
+package com.example.moviesot.login.loginScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.moviesot.R;
+
+public class loginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/moviesot/login/startScreen/startActivity.java
@@ -0,0 +1,15 @@
+package com.example.moviesot.login.startScreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.moviesot.R;
+
+public class startActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
